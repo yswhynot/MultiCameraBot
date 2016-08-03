754166cbbfd843ae7cd83fb8999bf2413eed646e
yswhynot <yswhynot@gmail.com>
2016-08-03 15:31:53 -0400

    update

--- multicam_calib/multicambot_gazebo/launch/image_view.sh
+++ multicam_calib/multicambot_gazebo/launch/image_view.sh
@@ -1,5 +1,8 @@
-rosrun image_view image_view image:=/multicambot/camera_left1/image_raw
-rosrun image_view image_view image:=/multicambot/camera_left0/image_raw
-rosrun image_view image_view image:=/multicambot/camera_front/image_raw
+# rosrun image_view image_view image:=/multicambot/camera_left1/image_raw
+# rosrun image_view image_view image:=/multicambot/camera_left0/image_raw
+# rosrun image_view image_view image:=/multicambot/camera_front/image_raw
+# rosrun image_view image_view image:=/multicambot/camera_right0/image_raw
+# rosrun image_view image_view image:=/multicambot/camera_right1/image_raw
+
 rosrun image_view image_view image:=/multicambot/camera_right0/image_raw
-rosrun image_view image_view image:=/multicambot/camera_right1/image_raw
+rosrun image_view image_view image:=/multicambot/camera_left0/image_raw
